6934fed804187e507f28a9e0604e7acdfd596923
orivoir <sam.gabor@hotmail.com>
2024-11-24 18:37:31 +0100

    clean entry point library

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 /**
-
-1 - draw board
-
-  - 1.a build board (8x8)
-
+ * For the therms `client` inside all code files take reference to developers that use
+ * this library and the therms `user` is a reference to final user that consume a application
+ * that import this library (in other therms `user` is probably a player).
  */
